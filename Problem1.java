80a24a6d9c226129d7ead4bd70e03374e56b2b60
kanchi.kodikara256@gmail.com <kanchi.kodikara256@gmail.com>
2020-02-26 06:09:08 +0530

    Added draft implementation to the file

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,22 @@
+class Problem1{
 
+	public static void main(String []args){
+		
+		int k=17;
+		Integer arr[]= {10, 15, 3, 7};	
+		System.out.println(Problem1.ifReturnSameValue(k,arr));
+
+	}
+
+	private static boolean ifReturnSameValue(Integer total, Integer arr[]){
+
+		for(int val:arr){
+
+			
+
+		}
+
+
+		return false;
+	}
+}
